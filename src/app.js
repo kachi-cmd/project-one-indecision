0e9c27d070695c261e7ddea6476f4ff1bc7e9242
emmanuel-kachi <emmagrt1@gmail.com>
2020-10-13 11:08:41 +0100

    all u need fir d app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Approuter from './routers/Approuter';
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+
+ReactDOM.render(<Approuter /> , document.getElementById('app'));
